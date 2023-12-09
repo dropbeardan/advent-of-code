fa1c14e2c7841ae51d1f9b5ca44894a7494ec93f
dropbeardan <dan.ess.001@gmail.com>
2023-12-09 13:06:52 +1100

    REFACTOR: Update number finding strategy to suit Day 1 part 2 exercise

--- 2023/day-1/helpers.test.ts
+++ 2023/day-1/helpers.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { getCalibrationValue } from './helpers';
+import { getCalibrationValue, getInstancesOfNumbers } from './helpers';
 
 describe('getCalibrationValue', () => {
 	it('returns the number represenatation of the first and last numbers in the line joined together', () => {
@@ -20,6 +20,97 @@ describe('getCalibrationValue', () => {
 			expect(() => getCalibrationValue('abc')).toThrow(
 				'No number detected in line.'
 			);
+		});
+	});
+});
+
+describe('getInstancesOfNumbers', () => {
+	it('returns the first index, last index, number representation and string representation of the number value in the string', () => {
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 0,
+			lastIndex: 0,
+			numberRepresentation: 0,
+			stringRepresentation: '0',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 1,
+			lastIndex: 1,
+			numberRepresentation: 1,
+			stringRepresentation: '1',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 2,
+			lastIndex: 2,
+			numberRepresentation: 2,
+			stringRepresentation: '2',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 3,
+			lastIndex: 3,
+			numberRepresentation: 3,
+			stringRepresentation: '3',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 4,
+			lastIndex: 4,
+			numberRepresentation: 4,
+			stringRepresentation: '4',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 5,
+			lastIndex: 5,
+			numberRepresentation: 5,
+			stringRepresentation: '5',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 6,
+			lastIndex: 6,
+			numberRepresentation: 6,
+			stringRepresentation: '6',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 7,
+			lastIndex: 7,
+			numberRepresentation: 7,
+			stringRepresentation: '7',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 8,
+			lastIndex: 8,
+			numberRepresentation: 8,
+			stringRepresentation: '8',
+		});
+
+		expect(getInstancesOfNumbers('0123456789')).toContainEqual({
+			firstIndex: 9,
+			lastIndex: 9,
+			numberRepresentation: 9,
+			stringRepresentation: '9',
+		});
+	});
+
+	describe('when there are multiple instances of the same number', () => {
+		it('sets the firstIndex and lastIndex to the indexes of the first and last instances of the number', () => {
+			expect(getInstancesOfNumbers('0x0x0')).toContainEqual({
+				firstIndex: 0,
+				lastIndex: 4,
+				numberRepresentation: 0,
+				stringRepresentation: '0',
+			});
+		});
+	});
+
+	describe('when there are no instances of any numbers', () => {
+		it('returns an empty array', () => {
+			expect(getInstancesOfNumbers('abc')).toEqual([]);
 		});
 	});
 });
--- 2023/day-1/helpers.ts
+++ 2023/day-1/helpers.ts
@@ -1,18 +1,53 @@
-export const getCalibrationValue = (line: string) => {
-	const firstNumberMatcher = /^[^\d]*(?<firstNumber>\d).*$/;
-	const firstNumberMatches = line.match(firstNumberMatcher);
+import { first, isEmpty, last, sortBy } from 'lodash';
 
-	if (!firstNumberMatches) {
+const NUMBER_MAPPINGS = {
+	'0': 0,
+	'1': 1,
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+};
+
+export const getCalibrationValue = (line: string) => {
+	const numberInstances = getInstancesOfNumbers(line);
+
+	if (isEmpty(numberInstances)) {
 		throw new Error('No number detected in line.');
 	}
 
-	const lastNumberMatcher = /^.*(?<lastNumber>\d)[^\d]*$/;
-	const lastNumberMatches = line.match(lastNumberMatcher);
+	const numberInstancesByFirstOccurrence = sortBy(
+		numberInstances,
+		'firstIndex'
+	);
+	const numberInstancesByLastOccurrence = sortBy(numberInstances, 'lastIndex');
 
-	const firstNumber = firstNumberMatches.groups!.firstNumber;
-	const lastNumber = lastNumberMatches
-		? lastNumberMatches.groups!.lastNumber
-		: firstNumber;
+	const firstNumber = first(
+		numberInstancesByFirstOccurrence
+	)!.numberRepresentation;
 
-	return Number(firstNumber + lastNumber);
+	const lastNumber = last(
+		numberInstancesByLastOccurrence
+	)!.numberRepresentation;
+
+	return Number(String(firstNumber) + String(lastNumber));
 };
+
+export const getInstancesOfNumbers = (line: string) =>
+	Object.entries(NUMBER_MAPPINGS)
+		.map(([stringRepresentation, numberRepresentation]) => {
+			const firstIndex = line.indexOf(stringRepresentation);
+			const lastIndex = line.lastIndexOf(stringRepresentation);
+
+			return {
+				firstIndex,
+				lastIndex,
+				numberRepresentation,
+				stringRepresentation,
+			};
+		})
+		.filter((instance) => instance.firstIndex > -1);
